1865a0969204ba9a310420e9e721bc1d8cccdca6
David Falcón Naranjo <david_el_125@hotmail.com>
2016-06-27 20:19:32 +0100

    Green: when_whe_update_the_day_and_there_is_no_more_days_left_quality_should_be_decreased_double

--- src/test/java/NormalItemTest.java
+++ src/test/java/NormalItemTest.java
@@ -28,5 +28,13 @@ public class NormalItemTest {
         assertThat(item.getQuality(), is(0));
     }
 
+    @Test public void
+    when_whe_update_the_day_and_there_is_no_more_days_left_quality_should_be_decreased_double() throws Exception {
+        Item item = new Item(MONGOOSE, 0, 8);
+        GildedRose gildedRose = new GildedRose(asList(item));
 
+        gildedRose.updateQuality();
+
+        assertThat(item.getQuality(), is(6));
+    }
 }
