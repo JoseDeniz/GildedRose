fb2b095d845f95b68d6bbc9637e3f9235a982f42
David Falcón Naranjo <david_el_125@hotmail.com>
2016-06-27 19:46:58 +0100

    Refactor: extract constant

--- src/test/java/BackstagePassesItemTest.java
+++ src/test/java/BackstagePassesItemTest.java
@@ -6,14 +6,17 @@ import static org.junit.Assert.assertThat;
 
 public class BackstagePassesItemTest {
 
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     /**
      * <= 5 > 0 --> quality += 3
      * <= 0 --> quality = 0
      */
 
-    @Test public void
+    @Test
+    public void
     when_whe_update_the_day_and_there_are_more_than_ten_days_left_quality_should_be_increased_by_one() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 20, 4);
+        Item item = new Item(BACKSTAGE_PASSES, 20, 4);
         GildedRose gildedRose = new GildedRose(asList(item));
 
         gildedRose.updateQuality();
@@ -21,14 +24,14 @@ public class BackstagePassesItemTest {
         assertThat(item.getQuality(), is(5));
     }
 
-    @Test public void
+    @Test
+    public void
     when_whe_update_the_day_and_there_are_more_than_five_and_less_than_ten_quality_should_be_increased_by_two() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 4);
+        Item item = new Item(BACKSTAGE_PASSES, 8, 4);
         GildedRose gildedRose = new GildedRose(asList(item));
 
         gildedRose.updateQuality();
 
         assertThat(item.getQuality(), is(6));
-
     }
 }
