e77c2744562917757c2061c595615fdbc0d8b102
David Falcón Naranjo <david_el_125@hotmail.com>
2016-06-27 19:41:05 +0100

    Green when_whe_update_the_day_and_there_are_more_than_five_and_less_than_ten_quality_should_be_increased_by_two

--- src/test/java/BackstagePassesItemTest.java
+++ src/test/java/BackstagePassesItemTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertThat;
 public class BackstagePassesItemTest {
 
     /**
-     * <= 10 && >= 5 --> quality += 2
      * <= 5 > 0 --> quality += 3
      * <= 0 --> quality = 0
      */
@@ -22,4 +21,14 @@ public class BackstagePassesItemTest {
         assertThat(item.getQuality(), is(5));
     }
 
+    @Test public void
+    when_whe_update_the_day_and_there_are_more_than_five_and_less_than_ten_quality_should_be_increased_by_two() {
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 4);
+        GildedRose gildedRose = new GildedRose(asList(item));
+
+        gildedRose.updateQuality();
+
+        assertThat(item.getQuality(), is(6));
+
+    }
 }
