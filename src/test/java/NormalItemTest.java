c6485ce26163a07c7ebbcbce1c727d607e9d0c12
David Falcón Naranjo <david_el_125@hotmail.com>
2016-06-27 19:11:18 +0100

    refactor: extract constant

--- src/test/java/NormalItemTest.java
+++ src/test/java/NormalItemTest.java
@@ -6,11 +6,13 @@ import static org.junit.Assert.assertThat;
 
 public class NormalItemTest {
 
-    private String MONGOOSE = "Elixir of the Mongoose";
+    private static final int SELL_IN = 5;
+    private static final int QUALITY = 7;
+    private static final String MONGOOSE = "Elixir of the Mongoose";
 
     @Test
     public void When_we_update_the_day_normal_item_should_be_decreased() {
-        Item item = new Item(MONGOOSE, 5, 7);
+        Item item = new Item(MONGOOSE, SELL_IN, QUALITY);
         GildedRose gildedRose = new GildedRose(asList(item));
 
         gildedRose.updateQuality();
--- src/test/java/SulfurasItemTest.java
+++ src/test/java/SulfurasItemTest.java
@@ -7,17 +7,19 @@ import static org.junit.Assert.assertThat;
 
 public class SulfurasItemTest {
 
-    private String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final int SELL_IN = 0;
+    private static final int QUALITY = 80;
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
-	@Test
-	public void when_we_update_the_day_sulfuras_quality_should_not_change() {
-        Item item = new Item(SULFURAS, 0, 80);
+    @Test
+    public void when_we_update_the_day_sulfuras_quality_should_not_change() {
+        Item item = new Item(SULFURAS, SELL_IN, QUALITY);
         GildedRose gildedRose = new GildedRose(asList(item));
 
         gildedRose.updateQuality();
 
-        assertThat(item.getSellIn(),is(0));
-        assertThat(item.getQuality(),is(80));
+        assertThat(item.getSellIn(), is(0));
+        assertThat(item.getQuality(), is(80));
     }
 
 }
